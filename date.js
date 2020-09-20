5adba238126e7a4c0a9773deadec3f67be78cd03
essanz <escobar1684@gmail.com>
2020-09-20 00:04:20 -0600

    Added hr tag

--- date.js
+++ date.js
@@ -1,10 +1,2 @@
-//let modified = document.lastModified;
-//document.getElementById("lastmodified").textContent = modified;
-let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric"
-
-}
-document.getElementById("lastmodified").textContent = new Date(document.lastModified).toLocaleDateString("en-US", options);
+let modified = document.lastModified;
+document.getElementById("lastmodified").textContent = modified;
