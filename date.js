eff75d4a67424334b5ec9df066487e7ff525aa70
essanz <escobar1684@gmail.com>
2020-09-20 00:44:34 -0600

    Fixed time

--- date.js
+++ date.js
@@ -1,9 +1,10 @@
-try {
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric"
-    };
-    document.getElementById("currentdate").innerHTML = new Date().toLocaleDateString("en-US", options);
+//let modified = document.lastModified;
+//document.getElementById("lastmodified").textContent = modified;
+let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+
 }
+document.getElementById("lastmodified").textContent = new Date(document.lastModified).toLocaleDateString("en-US", options);
